865de29738149769a2227d49f481dfc5d2cca017
Ben <119703246+walthermedia@users.noreply.github.com>
2023-10-19 13:56:24 -0500

    Update homebrew.sh

--- homebrew.sh
+++ homebrew.sh
@@ -1,8 +1,10 @@
-#!/usr/bin/env bash
+#!/usr/bin/env zsh
 
 if test ! $(which brew); then
     echo "Installing homebrew..."
     ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> ~/.zprofile
+    eval "$(/opt/homebrew/bin/brew shellenv)"
 fi
   
 brew update
