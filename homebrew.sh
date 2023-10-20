1d6a123b1c5f53646691a52917936e1d3c40c6c2
Ben <119703246+walthermedia@users.noreply.github.com>
2023-10-20 09:41:08 -0500

    Update homebrew.sh

--- homebrew.sh
+++ homebrew.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env zsh
 
+# Check for Oh My Zsh and install if we don't have it
+if test ! $(which omz); then
+  /bin/sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/HEAD/tools/install.sh)"
+fi
+
+# Check for Homebrew and install if we don't have it
 if test ! $(which brew); then
     echo "Installing homebrew..."
     ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
